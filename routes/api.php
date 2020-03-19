f7bda1a67fe4513aea4d6812b94218a7a09a4b8f
dothang301092 <dothang301092@gmail.com>
2020-03-19 22:24:42 +0700

    add Api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('users', 'Api\UserController@index');
+Route::post('users', 'Api\UserController@store');
+Route::get('users/{id}', 'Api\UserController@show');
