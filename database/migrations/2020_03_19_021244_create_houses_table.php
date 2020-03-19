c6678a79771fc8dc70d39eba76c4146e0c3a4030
dothang301092 <dothang301092@gmail.com>
2020-03-19 10:00:31 +0700

    first

---
+++ database/migrations/2020_03_19_021244_create_houses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_house');
+            $table->string('house_type');
+            $table->string('address');
+            $table->integer('bed_room_num');
+            $table->integer('bath_room_num');
+            $table->string('description');
+            $table->bigInteger('price');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
