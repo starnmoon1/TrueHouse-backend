f1e637f2f124dce68bd934cbc67a3c1a11810edf
dothang301092 <dothang301092@gmail.com>
2020-03-30 12:09:39 +0700

    finally

--- app/House.php
+++ app/House.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class House extends Model
 {
-    //
+    public function comments()
+    {
+        return $this->hasMany('App\Comment', 'house_id');
+    }
 }
--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\House;
 use App\Http\Controllers\Controller;
 use App\Http\Services\Impl\CommentService;
 use Illuminate\Http\Request;
@@ -27,5 +28,30 @@ class CommentController extends Controller
     public function show($id)
     {
         return $this->commentService->findById($id);
+    }
+
+    public function showRating($id)
+    {
+        Try {
+            $comments = House::find($id)->comments;
+            $sumStar = 0;
+            $countComment = 0;
+            $avgStar = 0;
+            foreach ($comments as $comment) {
+                $countComment += 1;
+                $sumStar += $comment->rating;
+            }
+            if ($countComment > 0) {
+                $avgStar = $sumStar / $countComment;
+            }
+            $data = ['status' => 'success',
+                'data' => $avgStar];
+            return response()->json($data, 201);
+        } catch (\Exception $exception) {
+            $data = ['status' => 'errors',
+                'message' => $exception];
+            return response()->json($data, 501);
+        }
+
     }
 }
